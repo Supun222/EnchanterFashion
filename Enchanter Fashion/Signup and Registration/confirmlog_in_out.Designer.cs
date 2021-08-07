b0019c7d7a238b7da3d5e5a53af9e817a530bab6
Supun222 <74459467+Supun222@users.noreply.github.com>
2021-08-07 19:57:37 +0530

    ado no

---
+++ Enchanter Fashion/Signup and Registration/confirmlog_in_out.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Enchanter_Fashion.Signup_and_Registration
+{
+    partial class confirmlog_in_out
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(confirmlog_in_out));
+            this.label3 = new System.Windows.Forms.Label();
+            this.paswd_txt = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.bunifuPanel2 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.confirm_btn = new MaterialSkin.Controls.MaterialFlatButton();
+            this.bunifuPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Century Schoolbook", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(36, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 15);
+            this.label3.TabIndex = 102;
+            this.label3.Text = "Enter the password\r\n";
+            // 
+            // paswd_txt
+            // 
+            this.paswd_txt.Depth = 0;
+            this.paswd_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(179)))), ((int)(((byte)(132)))));
+            this.paswd_txt.Hint = "";
+            this.paswd_txt.Location = new System.Drawing.Point(53, 88);
+            this.paswd_txt.MaxLength = 32767;
+            this.paswd_txt.MouseState = MaterialSkin.MouseState.HOVER;
+            this.paswd_txt.Name = "paswd_txt";
+            this.paswd_txt.PasswordChar = '\0';
+            this.paswd_txt.SelectedText = "";
+            this.paswd_txt.SelectionLength = 0;
+            this.paswd_txt.SelectionStart = 0;
+            this.paswd_txt.Size = new System.Drawing.Size(176, 23);
+            this.paswd_txt.TabIndex = 101;
+            this.paswd_txt.TabStop = false;
+            this.paswd_txt.UseSystemPasswordChar = false;
+            // 
+            // bunifuPanel2
+            // 
+            this.bunifuPanel2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(226)))), ((int)(((byte)(222)))));
+            this.bunifuPanel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel2.BackgroundImage")));
+            this.bunifuPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel2.BorderColor = System.Drawing.Color.Transparent;
+            this.bunifuPanel2.BorderRadius = 3;
+            this.bunifuPanel2.BorderThickness = 1;
+            this.bunifuPanel2.Controls.Add(this.button2);
+            this.bunifuPanel2.Controls.Add(this.button3);
+            this.bunifuPanel2.Controls.Add(this.label2);
+            this.bunifuPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bunifuPanel2.Location = new System.Drawing.Point(0, 0);
+            this.bunifuPanel2.Name = "bunifuPanel2";
+            this.bunifuPanel2.ShowBorders = true;
+            this.bunifuPanel2.Size = new System.Drawing.Size(387, 45);
+            this.bunifuPanel2.TabIndex = 103;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(339, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 22);
+            this.button2.TabIndex = 95;
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(286, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 22);
+            this.button3.TabIndex = 94;
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "E n c h a n t e r   F a s h i o n ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(32, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 104;
+            this.label1.Text = "Enter the password";
+            // 
+            // confirm_btn
+            // 
+            this.confirm_btn.AutoSize = true;
+            this.confirm_btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.confirm_btn.BackColor = System.Drawing.Color.White;
+            this.confirm_btn.Depth = 0;
+            this.confirm_btn.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.confirm_btn.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.confirm_btn.Icon = null;
+            this.confirm_btn.Location = new System.Drawing.Point(272, 88);
+            this.confirm_btn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.confirm_btn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.confirm_btn.Name = "confirm_btn";
+            this.confirm_btn.Primary = false;
+            this.confirm_btn.Size = new System.Drawing.Size(74, 36);
+            this.confirm_btn.TabIndex = 105;
+            this.confirm_btn.Text = "Logout";
+            this.confirm_btn.UseVisualStyleBackColor = false;
+            this.confirm_btn.Click += new System.EventHandler(this.confirm_btn_Click);
+            // 
+            // confirmlog_in_out
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 152);
+            this.Controls.Add(this.confirm_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bunifuPanel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.paswd_txt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "confirmlog_in_out";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "confirmlog_in_out";
+            this.bunifuPanel2.ResumeLayout(false);
+            this.bunifuPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        public MaterialSkin.Controls.MaterialSingleLineTextField paswd_txt;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        public MaterialSkin.Controls.MaterialFlatButton confirm_btn;
+        public System.Windows.Forms.Label label1;
+    }
+}
---
+++ Enchanter Fashion/Signup and Registration/confirmlog_in_out.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enchanter_Fashion.Signup_and_Registration
+{
+    public partial class confirmlog_in_out : Form
+    {
+        public confirmlog_in_out()
+        {
+            InitializeComponent();
+        }
+
+        private void confirm_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
