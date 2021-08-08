491684e3fb76219f341fce10064dd78e3c55dcf9
Pavith <68535335+PavithMadushan@users.noreply.github.com>
2021-08-08 16:25:38 +0530

    again

--- Enchanter Fashion/inventory/UserControls/uc_items.cs
+++ Enchanter Fashion/inventory/UserControls/uc_items.cs
@@ -61,6 +61,192 @@ namespace Enchanter_Fashion.inventory.UserControls
 
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+
+
+                conn.Open();
+
+                String str = "Select bill_no,item_id,item_name,unit_price,quantity From sales Where bill_no = '" + txt_bill.Text + "'";
+
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+
+
+                cmd.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                da.Fill(dt);
+
+                dgvUser.DataSource = dt;
+
+
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    txt_bill.Text = dr.GetValue(0).ToString();
+                    txt_item.Text = dr.GetValue(1).ToString();
+                    txt_name.Text = dr.GetValue(2).ToString();
+                    txt_price.Text = dr.GetValue(3).ToString();
+
+                    txt_qty.Text = dr.GetValue(4).ToString();
+
+                   
+                }
+
+                conn.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+
+
+                conn.Open();
+
+                String str = "Select bill_no,item_id,item_name,unit_price,quantity From sales Where bill_no = '" + txt_item.Text + "'";
+
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+
+
+                cmd.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                da.Fill(dt);
+
+                dgvUser.DataSource = dt;
+
+
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    txt_bill.Text = dr.GetValue(0).ToString();
+                    txt_item.Text = dr.GetValue(1).ToString();
+                    txt_name.Text = dr.GetValue(2).ToString();
+                    txt_price.Text = dr.GetValue(3).ToString();
+
+                    txt_qty.Text = dr.GetValue(4).ToString();
+
+
+                }
+
+                conn.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                if (txt_bill.Text == "" || txt_item.Text == "" || txt_name.Text == "" || txt_price.Text == "" || txt_qty.Text=="")
+                {
+                    MessageBox.Show("Please , Insert all Information ... ", "Try Again", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    //MySqlConnection con = new MySqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\hp\documents\visual studio 2010\Projects\Inventory Managment System in csharp\Inventory Managment System in csharp\IMS.mdf;Integrated Security=True;User Instance=True");
+
+                    conn.Open();
+
+                    String str = "Insert Into sales (bill_no,item_id,item_name,unit_price,quantity) Values ('" + txt_bill.Text + "','" + txt_item.Text + "','" + txt_name.Text + "','" + txt_price.Text + "','" + txt_qty.Text + "')";
+
+                    MySqlCommand cmd = new MySqlCommand(str, conn);
+
+                    cmd.ExecuteNonQuery();
+
+                    String str2 = "Select max(bill_no) From sales";
+
+                    MySqlCommand cmd2 = new MySqlCommand(str2, conn);
+
+                    MySqlDataReader dr = cmd2.ExecuteReader();
+
+                    if (dr.Read())
+                    {
+                      //  showdata();
+
+                        MessageBox.Show("Customers Created Successfull ....", "Thank You", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        clear();
+
+                       // auto();
+
+                        conn.Close();
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+
+
+        }
+
+
+        private void clear()
+        {
+            txt_bill.Text = "";
+            txt_item.Text = "";
+            txt_name.Text = "";
+            txt_price.Text = "";
+
+            txt_qty.Text = "";
+
+            
+
+        }
 
 
     }
