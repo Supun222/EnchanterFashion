a26c1e57f89837b9581128b9a8c6cf26cf7ca0e0
Pavith <68535335+PavithMadushan@users.noreply.github.com>
2021-08-08 10:19:01 +0530

    10.18am pavith

--- Enchanter Fashion/inventory/UserControls/uc_categories.cs
+++ Enchanter Fashion/inventory/UserControls/uc_categories.cs
@@ -62,9 +62,133 @@ namespace Enchanter_Fashion.inventory.UserControls
 
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                conn.Open();
+
+                String str = "Select item_id,item_name,item_type,itm_quantity,color,size,brand,price,season,stock_id From contain_items Where item_id = '" + supplierIdTb.Text + "'";
+
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+
+
+                cmd.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                da.Fill(dt);
+
+                dgvUser.DataSource = dt;
 
 
 
 
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    supplierIdTb.Text = dr.GetValue(0).ToString();
+                    companyNameTb.Text = dr.GetValue(1).ToString();
+                    addressTb.Text = dr.GetValue(2).ToString();
+                    txt_qty.Text = dr.GetValue(3).ToString();
+
+                    txt_color.Text = dr.GetValue(4).ToString();
+                    txt_size.Text = dr.GetValue(5).ToString();
+                    txt_brand.Text = dr.GetValue(6).ToString();
+                    txt_price.Text = dr.GetValue(7).ToString();
+                    txt_season.Text = dr.GetValue(8).ToString();
+
+                }
+
+                conn.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                conn.Open();
+
+                String str = "Select item_id,item_name,item_type,itm_quantity,color,size,brand,price,season,stock_id From contain_items Where item_name = '" + companyNameTb.Text + "'";
+
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+
+
+                cmd.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                da.Fill(dt);
+
+                dgvUser.DataSource = dt;
+
+
+
+
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    supplierIdTb.Text = dr.GetValue(0).ToString();
+                    companyNameTb.Text = dr.GetValue(1).ToString();
+                    addressTb.Text = dr.GetValue(2).ToString();
+                    txt_qty.Text = dr.GetValue(3).ToString();
+
+                    txt_color.Text = dr.GetValue(4).ToString();
+                    txt_size.Text = dr.GetValue(5).ToString();
+                    txt_brand.Text = dr.GetValue(6).ToString();
+                    txt_price.Text = dr.GetValue(7).ToString();
+                    txt_season.Text = dr.GetValue(8).ToString();
+
+                }
+
+                conn.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+        private void clear()
+        {
+            supplierIdTb.Text = "";
+            companyNameTb.Text = "";
+            addressTb.Text = "";
+            txt_qty.Text = "";
+
+            txt_color.Text = "";
+            txt_size.Text = "";
+            txt_brand.Text = "";
+            txt_price.Text = "";
+            txt_season.Text = "";
+
+        }
     }
 }
